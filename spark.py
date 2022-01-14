131d895d39a926a8712fec69332e8783ae6c2d71
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2022-01-14 10:48:22 +0100

    update to Python 3

--- spark.py
+++ spark.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 
 """
-spark.py
-~~~~~~~~
+Spark
+================================================================================
 
-A port of @holman's spark project for Python.
+A port of @holman's [spark] project for Python 3.
+
+[spark]: https://github.com/holman/spark
 """
 
 import sys
 
-ticks = u' ▁▂▃▄▅▆▇█'
+ticks = " ▁▂▃▄▅▆▇█"
 
 
 def spark_string(ints, fit_min=False):
@@ -23,28 +25,30 @@ def spark_string(ints, fit_min=False):
     min_range = min(ints) if fit_min else 0
     step_range = max(ints) - min_range
     step = (step_range / float(len(ticks) - 1)) or 1
-    return u''.join(ticks[int(round((i - min_range) / step))] for i in ints)
+    return "".join(ticks[int(round((i - min_range) / step))] for i in ints)
 
 
 def spark_print(ints, stream=None, fit_min=False):
     """Prints spark to given stream."""
     if stream is None:
         stream = sys.stdout
-    stream.write(spark_string(ints, fit_min=fit_min).encode('utf-8'))
+    stream.write(spark_string(ints, fit_min=fit_min) + "\n")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     if len(sys.argv) > 1:
-        sparks = map(int, sys.argv[1:])
+        sparks = [int(arg) for arg in sys.argv[1:]]
         spark_print(sparks)
         print
     else:
-        print "spark\n"
-        print "USAGE:"
-        print "  spark.py [spaces separated values]\n"
-        print "EXAMPLES:"
-        print "  spark.py 1 5 22 13 53"
+        print("Spark: ▁▂▃▅▂▇ in your shell")
+        print()
+        print("Usage:")
+        print("  python spark.py [spaces separated values]\n")
+        print("Examples:")
+        print("  python spark.py 1 5 22 13 53")
         spark_print([1, 5, 22, 13, 53])
-        print
-        print "  spark.py 0 30 55 80 33 150"
+        print()
+        print("  python spark.py 0 30 55 80 33 150")
         spark_print([0, 30, 55, 80, 33, 150])
-        print
+        print()
